11d7f1a9b67260aa132d78eeeba5890ff237ab60
Kunj-Choksi <kunj07soni@gmail.com>
2024-06-07 01:40:04 -0500

    User information

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  validates :full_name, length: { maximum: 50 }
+  validates :body, length: { maximum: 80 }
+  validate :valid_username
+
+  def valid_username
+    return unless self.will_save_change_to_username?
+
+    errors.add(:username, "is already taken") if username.changed? User.exists?(username: username)
+
+    restricted_username_list = %w[admin root dashboard analytics appearance settings preferences calender users]
+
+    errors.add(:username, "is restricted to use") if restricted_username_list.include?(username)
+  end
 end
